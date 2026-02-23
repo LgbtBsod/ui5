4f46dbd394b98e27da7bc8b952025e3661009b46
LgbtBsod <98397482+LgbtBsod@users.noreply.github.com>
2026-02-24 01:48:11 +0400

    Elevate UI wrapper with premium visual system and richer pages

---
+++ controller/Object.controller.js
@@ -0,0 +1,41 @@
+sap.ui.define([
+  "sap/ui/core/mvc/Controller"
+], function (Controller) {
+  "use strict";
+
+  return Controller.extend("theme7.controller.Object", {
+
+    onInit: function () {
+      this.getOwnerComponent().getRouter()
+        .getRoute("object")
+        .attachPatternMatched(this._onMatched, this);
+    },
+
+    _onMatched: function (oEvent) {
+      var sId = oEvent.getParameter("arguments").id;
+
+      this.getOwnerComponent().getModel("stateModel").setProperty("/selectedId", sId);
+      this.getView().getParent().setLayout("TwoColumnsMidExpanded");
+    },
+
+    onOpenDetail: function () {
+      var sId = this.getOwnerComponent().getModel("stateModel").getProperty("/selectedId") || "1001";
+
+      this.getOwnerComponent().getRouter().navTo("detail", { id: sId });
+    },
+
+    onToggleEdit: function (oEvent) {
+      var bOn = oEvent.getParameter("state");
+      var oStateModel = this.getOwnerComponent().getModel("stateModel");
+
+      oStateModel.setProperty("/mode", bOn ? "EDIT" : "READ");
+
+      if (bOn) {
+        document.body.classList.add("editMode");
+      } else {
+        document.body.classList.remove("editMode");
+      }
+    }
+
+  });
+});
