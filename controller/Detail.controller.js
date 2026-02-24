9b0531061755c772b337ee3ee9df54c372df11ae
LgbtBsod <98397482+LgbtBsod@users.noreply.github.com>
2026-02-24 13:29:13 +0400

    Fix theme UX and enable inline detail edit mode

--- controller/Detail.controller.js
+++ controller/Detail.controller.js
@@ -1,6 +1,8 @@
 sap.ui.define([
-  "sap_ui5/controller/Base.controller"
-], function (BaseController) {
+  "sap_ui5/controller/Base.controller",
+  "sap_ui5/service/backend/BackendAdapter",
+  "sap/m/MessageToast"
+], function (BaseController, BackendAdapter, MessageToast) {
   "use strict";
 
   return BaseController.extend("sap_ui5.controller.Detail", {
@@ -12,6 +14,7 @@ sap.ui.define([
     _onMatched: function (oEvent) {
       var sId = oEvent.getParameter("arguments").id;
       var sLayout = oEvent.getParameter("arguments").layout || "TwoColumnsMidExpanded";
+      var oStateModel = this.getModel("state");
       var oDataModel = this.getModel("data");
       var aChecklists = oDataModel.getProperty("/checkLists") || [];
       var oSelected = aChecklists.find(function (item) {
@@ -20,7 +23,8 @@ sap.ui.define([
 
       oDataModel.setProperty("/selectedChecklist", oSelected);
       this.getModel("selected").setData(oSelected || {});
-      this.getModel("state").setProperty("/layout", sLayout);
+      oStateModel.setProperty("/layout", sLayout);
+      oStateModel.setProperty("/mode", "READ");
     },
 
     onCloseDetail: function () {
@@ -29,18 +33,59 @@ sap.ui.define([
     },
 
     onToggleEditFromDetail: function (oEvent) {
-      if (!oEvent.getParameter("state")) {
+      var bEditMode = oEvent.getParameter("state");
+
+      if (!bEditMode) {
+        this.getModel("state").setProperty("/mode", "READ");
         return;
       }
 
-      var sId = (((this.getModel("selected").getData() || {}).root || {}).id || "");
+      this.getModel("state").setProperty("/mode", "EDIT");
+    },
+
+    onCancelEditFromDetail: function () {
+      var oSelected = this.getModel("data").getProperty("/selectedChecklist") || {};
+      this.getModel("selected").setData(JSON.parse(JSON.stringify(oSelected)));
+      this.getModel("state").setProperty("/mode", "READ");
+    },
+
+    onSaveDetail: function () {
+      var oStateModel = this.getModel("state");
+      var oDataModel = this.getModel("data");
+      var oBundle = this.getResourceBundle();
+      var oEdited = this.getModel("selected").getData() || {};
+      var sId = ((((oEdited || {}).root || {}).id) || "").trim();
+
       if (!sId) {
+        MessageToast.show("Checklist id not found");
         return;
       }
 
-      this.getModel("state").setProperty("/objectAction", "EDIT");
-      this.getModel("state").setProperty("/layout", "TwoColumnsMidExpanded");
-      this.navTo("object", { id: sId });
+      oStateModel.setProperty("/isBusy", true);
+
+      BackendAdapter.updateCheckList(sId, oEdited)
+        .then(function (oSavedChecklist) {
+          return BackendAdapter.getCheckLists().then(function (aUpdatedCheckLists) {
+            return {
+              savedChecklist: oSavedChecklist,
+              checkLists: aUpdatedCheckLists
+            };
+          });
+        })
+        .then(function (oResult) {
+          oDataModel.setProperty("/checkLists", oResult.checkLists);
+          oDataModel.setProperty("/visibleCheckLists", oResult.checkLists);
+          oDataModel.setProperty("/selectedChecklist", oResult.savedChecklist);
+          this.getModel("selected").setData(oResult.savedChecklist);
+          oStateModel.setProperty("/mode", "READ");
+          MessageToast.show(oBundle.getText("objectSaved"));
+        }.bind(this))
+        .catch(function (oError) {
+          MessageToast.show(oBundle.getText("objectSaveFailed", [((oError && oError.message) || "Unknown error")]));
+        })
+        .finally(function () {
+          oStateModel.setProperty("/isBusy", false);
+        });
     },
 
     resultText: function (bResult) {
