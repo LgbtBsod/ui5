34a32b4bce53a3acfce952a495ae3603f4045935
LgbtBsod <lgbt.bsod@yandex.ru>
2026-02-24 04:52:06 +0400

    м7

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -4,4 +4,15 @@ sap.ui.define([
   "use strict";
 
   return Controller.extend("sap_ui5.controller.App", {});
+
+onToggleTheme: function () {
+  const current = sap.ui.getCore().getConfiguration().getTheme();
+  const next = current === "sap_fiori_3"
+    ? "sap_fiori_3_dark"
+    : "sap_fiori_3";
+
+  sap.ui.getCore().applyTheme(next);
+  document.body.classList.toggle("appDark");
+}
+
 });
